f5418cdd67aef9df9050c80a8998abdb961b9d0d
zakiy380k <danilkadodi@gmail.com>
2026-02-13 18:59:54 +0200

    Update valentine.py

--- valentine.py
+++ valentine.py
@@ -1,20 +1,28 @@
 from threading import Thread
 from fastapi import FastAPI
 import uvicorn
-from telebot import TeleBot
+from telebot import TeleBot, types
+import threading
 
 bot = TeleBot("8488578422:AAEWZlmb5wmI5xc1QOyMaQeoo2TwUVIk5Gw")
 app = FastAPI()
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Бот работает")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    web_app = types.WebAppInfo("https://ТВОЙ-САЙТ.onrender.com")
+    btn = types.KeyboardButton("Открыть", web_app=web_app)
+    markup.add(btn)
+
+    bot.send_message(message.chat.id, "Нажми кнопку", reply_markup=markup)
+
 
 def run_bot():
+    print("Bot started")
     bot.infinity_polling()
 
-def run_api():
-    uvicorn.run(app, host="0.0.0.0", port=10000)
 
-Thread(target=run_bot).start()
-run_api()
+threading.Thread(target=run_bot).start()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=10000)
